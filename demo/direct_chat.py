5bde0112615228306e78a2160b7a492616b80060
kimaust <kim.aus@outlook.com>
2023-02-03 20:13:44 +0800

    Added more generic helper function and chatgpt preamble in the demo

--- demo/direct_chat.py
+++ demo/direct_chat.py
@@ -1,21 +1,23 @@
 import time
 
-from helpers import read_initial_prompt, read_bot_name
+from helpers import read_file
 from chatbot import User, Bot
 
 
 def main() -> None:
-    initial_prompt = read_initial_prompt()
+    chatgpt_preamble = read_file("chatgpt-preamble.txt")
+    chatgpt_preamble = f"{chatgpt_preamble}\nCurrent date: {time.strftime('%Y-%m-%d')}"
+    initial_prompt = read_file("initial-prompt.txt")
 
     # Add the bot name and username to the initial prompt.
     # You can remove this if you alreacy have the bot name and username in the initial prompt.
     username = input("What is your name? ")
-    bot_name = read_bot_name()
-    initial_prompt = f"{initial_prompt}\nI want you to act as if your name is {bot_name}.\n" \
-                          f"And pretend you are talking to a person called {username}."
+    bot_name = read_file("bot-name.txt")
+    bot_initial_prompt = f"{chatgpt_preamble}\n\n{initial_prompt}\nI want you to act as if your name is {bot_name}.\n" \
+        f"And pretend you are talking to a person called {username}."
 
     user = User(username)
-    bot = Bot(bot_name, initial_prompt)
+    bot = Bot(bot_name, bot_initial_prompt)
 
     while True:
         prompt = input(f"{user.name}> ").strip()
--- demo/helpers.py
+++ demo/helpers.py
@@ -10,21 +10,9 @@ def get_data_directory_path() -> str:
     return file_path
 
 
-def read_initial_prompt() -> str:
+def read_file(filename: str) -> str:
     data_directory_path = get_data_directory_path()
+    with open(f"{data_directory_path}/{filename}", "r", encoding="utf-8") as f:
+        content = f.read()
 
-    # Read the initial prompt to give to the bot.
-    with open(f"{data_directory_path}/initial_prompt.txt", "r", encoding="utf-8") as f:
-        initial_prompt = f.read()
-
-    return initial_prompt.strip()
-
-
-def read_bot_name() -> str:
-    data_directory_path = get_data_directory_path()
-
-    # Read the bot name.
-    with open(f"{data_directory_path}/bot_name.txt", "r", encoding="utf-8") as f:
-        bot_name = f.read()
-
-    return bot_name.strip()
+    return content.strip()
